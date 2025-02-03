50df7437ce3138f0acdfdc30e79a317c6e276924
evetovicsm <5862838+evetovicsm@users.noreply.github.com>
2025-02-03 12:21:41 +0100

    Update plugin.py

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -48,7 +48,7 @@ class Plugin(AAPlugin):
         lookup_attribute = self.plugin_configuration.get(PLUGIN_SECTION, 'lookup_attribute')
         bind_user = self.plugin_configuration.get(PLUGIN_SECTION, 'bind_user')
         bind_password = self.plugin_configuration.get(PLUGIN_SECTION, 'bind_password')
-        if not all([domain_controller, lookup_attribute, search_base, bind_username, bind_password]):
+        if not all([domain_controller, lookup_attribute, search_base, bind_user, bind_password]):
             self.logger.error("The following configuration items must be set: domain_controller, lookup_attribute, bind_user, bind_password")
         try:
             conn = ldap.initialize(f"ldap://{{domain_controller}}")
